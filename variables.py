275aa195f26596655c3d189f461bd91b0749ce34
lefrycarvajal <lefrycarvajal@gmail.com>
2023-09-04 22:12:17 -0400

    tarea variables codelab

--- variables.py
+++ variables.py
@@ -4,3 +4,19 @@ nf = float (input(prompt="Ahora inserta un numero decimal: "))
 trfls = bool (input(prompt="¿Pusiste los datos correctos? True/False "))
 
 print(f"Hola {nombre}, Hagamos una suma...", f"La suma de {ne}", f"+ {nf}", f"= {ne+nf}" sep="\n" )
+
+# Límites de enteros en Python
+'''
+entero_minimo = -2**31  # Valor mínimo para enteros con signo de 32 bits
+entero_maximo = 2**31 - 1  # Valor máximo para enteros con signo de 32 bits
+
+print("Límite mínimo de entero:", entero_minimo)
+print("Límite máximo de entero:", entero_maximo)
+
+Límites de números de punto flotante en Python
+flotante_minimo = 2.2250738585072014e-308  # Valor mínimo para números de punto flotante en doble precisión
+flotante_maximo = 1.7976931348623157e+308  # Valor máximo para números de punto flotante en doble precisión
+
+print("Límite mínimo de punto flotante:", flotante_minimo)
+print("Límite máximo de punto flotante:", flotante_maximo)
+'''
