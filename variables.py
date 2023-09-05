27af8f0783d890597aec471258c569aebf4d10fd
lefrycarvajal <lefrycarvajal@gmail.com>
2023-09-04 22:13:34 -0400

    tarea variables codelab

--- variables.py
+++ variables.py
@@ -7,16 +7,24 @@ print(f"Hola {nombre}, Hagamos una suma...", f"La suma de {ne}", f"+ {nf}", f"= 
 
 # Límites de enteros en Python
 '''
-entero_minimo = -2**31  # Valor mínimo para enteros con signo de 32 bits
-entero_maximo = 2**31 - 1  # Valor máximo para enteros con signo de 32 bits
+El valor mínimo para enteros con signo de 32 bits es igual a -2*31, mientras que el valor máximo es igual a 2*31 - 1.
 
+Para obtener estos límites, podemos utilizar las siguientes asignaciones:
+entero_minimo = -2**31
+entero_maximo = 2**31 - 1
+
+Y luego, podemos imprimir estos valores de la siguiente manera:
 print("Límite mínimo de entero:", entero_minimo)
 print("Límite máximo de entero:", entero_maximo)
 
 Límites de números de punto flotante en Python
-flotante_minimo = 2.2250738585072014e-308  # Valor mínimo para números de punto flotante en doble precisión
-flotante_maximo = 1.7976931348623157e+308  # Valor máximo para números de punto flotante en doble precisión
+En cuanto a los números de punto flotante en doble precisión, el valor mínimo es aproximadamente 2.2250738585072014e-308, mientras que el valor máximo es aproximadamente 1.7976931348623157e+308.
 
+Podemos representar estos límites de la siguiente manera:
+flotante_minimo = 2.2250738585072014e-308
+flotante_maximo = 1.7976931348623157e+308
+
+Y luego, para mostrarlos en pantalla, utilizamos las siguientes impresiones:
 print("Límite mínimo de punto flotante:", flotante_minimo)
 print("Límite máximo de punto flotante:", flotante_maximo)
 '''
